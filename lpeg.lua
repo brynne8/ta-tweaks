59fd023af3a9d54bea4f438ec906bd3325813ff8
Alexander Misel <7542439+AlexanderMisel@users.noreply.github.com>
2020-07-14 16:51:40 +0800

    lpeg lexer

---
+++ lpeg.lua
@@ -0,0 +1,41 @@
+-- Copyright 2006-2020 Mitchell mitchell.att.foicica.com. See License.txt.
+-- MediaWiki LPeg lexer.
+-- Contributed by Alexander Misel.
+
+local lexer = require('lexer')
+local token, word_match = lexer.token, lexer.word_match
+local P, R, S, B = lpeg.P, lpeg.R, lpeg.S, lpeg.B
+
+local lex = lexer.new('lpeg')
+
+-- Comments.
+lex:add_rule('comment', token(lexer.COMMENT, lexer.to_eol('--')))
+
+local name = (lexer.alpha + '_') * (lexer.alnum + '_')^0
+
+-- Classes
+local defined = P('%') * name
+local range = lexer.any * '-' * (lexer.any - ']')
+local item = defined + range + lexer.any
+lex:add_rule('class', token(lexer.DEFAULT, P('[') * P('^')^-1 * item * (-P(']') * item)^0 * ']'))
+
+-- Strings
+local sq_str = lexer.range("'")
+local dq_str = lexer.range('"')
+local str = token(lexer.STRING, sq_str + dq_str)
+lex:add_rule('string', str)
+
+-- Numbers.
+local number = token(lexer.NUMBER, lexer.number)
+lex:add_rule('number', number)
+
+-- Functions.
+lex:add_rule('rcap', token(lexer.OPERATOR, S('-=~') * '>') * lexer.space^0 * (number + str + token(lexer.FUNCTION, name)) )
+
+-- Operators.
+lex:add_rule('operator', token(lexer.OPERATOR, S('<-/&!+*=')))
+
+
+lex:add_rule('terminal', token(lexer.VARIABLE, name))
+
+return lex
